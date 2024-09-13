2f919a6f9be559da9c0b9651c88bf75f3e907bc5
Jimmy <Jimmy@Desktop-EW2TO>
2024-09-13 20:45:16 +0300

    Add project files.

---
+++ Tyuiu.FedotkinNE.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.FedotkinNE.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+            string Name = "Никита";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут " + Name + " мне " + age + " лет ");
+            Console.ReadKey();
+        }
+    }
+}
