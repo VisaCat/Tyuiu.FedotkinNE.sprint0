70e27c1ba3f1ba6eade47a730236cdafa8e8e1f4
Sir Nik <vikfed3717@gmail.com>
2024-09-14 11:34:38 +0300

    Task6

---
+++ Tyuiu.FedotkinNE.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.FedotkinNE.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.FedotkinNE.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bd229f38-42c3-48c5-87c5-f13e35768c90")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.FedotkinNE.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.FedotkinNE.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.FedotkinNE.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма Элементов Массива = " +  DataService.AdditionArray(arraynums));
+            Console.WriteLine("Разность Элементов Массива = " + DataService.SubstractionArray(arraynums));
+            Console.WriteLine("Произведение Элементов Массива = " + DataService.MultiplicationArray(arraynums));
+            Console.ReadKey();
+
+        }
+    }
+}
